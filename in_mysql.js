d65ff7fdd0f9382c1baa9f5611da98b50797e702
you21979 <you2197901@gmail.com>
2014-03-24 22:06:24 +0900

    config rename

--- in_mysql.js
+++ in_mysql.js
@@ -8,10 +8,10 @@ exports.run = function(inputTools, callback){
     var data = inputTools.kvs[args.input];
     var config = inputTools.config;
     var cl = mysql.createClient({
-        user: config.user,
-        password: config.password,
-        host: config.host,
-        database: config.database,
+        user: config.mysql_user,
+        password: config.mysql_password,
+        host: config.mysql_host,
+        database: config.mysql_database,
     });
     cl.query(args.sql, [], function(err, res){
         callback(err, res);
